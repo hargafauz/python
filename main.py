a40eb859aab5b55adf829e122e4c120494c4b5ac
hargafauz <mohammadfauz2@gmail.com>
2022-08-24 12:31:43 +0300

    inial

---
+++ main.py
@@ -0,0 +1,8 @@
+name = input("what is your name ")
+age =  input("what is your age ")
+nationality =  input("what is your nationality ")
+
+print(f"name {name} \n",f"age {age} \n",f"nationality {nationality} \n")
+
+
+
