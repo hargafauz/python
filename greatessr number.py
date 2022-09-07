7e9be5b53908ef14bb31eee1cd9e1cb88acb0c5a
hargafauz <mohammadfauz2@gmail.com>
2022-09-07 11:29:50 +0300

    wed

---
+++ greatessr number.py
@@ -0,0 +1,16 @@
+n1 = int (input("enter the first number: "))
+n2 = int(input("enter the second number: "))
+n3 = int(input("enter the third num ber: "))
+if ( (n1 > n2) & (n2 > n3) ) :
+    print ("n1 is big ::,n1")
+elif ( (n2 > n3) ) :
+    print ("n2 is big ::,n2")
+else :
+    print ("n3 is big ::,n3")
+    
+ 
+    
+
+       
+    
+            
